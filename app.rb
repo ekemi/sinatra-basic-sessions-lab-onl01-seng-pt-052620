ce8a4167ce5efb2701c566a4c4aab48c3f29b1b1
edmundo papi <edmundopierre@yahoo.com>
2020-09-19 06:21:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+get '/' do
+
+  erb :index
+end
+
+post '/checkout' do
+item = params[:item]
+@session = session
+
+ @session[:item] = item
+ @session
 end
